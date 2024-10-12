4530d3d1b5b173b51ae31c966265730cf6132e96
Utsuhoagie <thiendangssb@gmail.com>
2024-10-12 22:31:49 +0700

    [init] Player, Block and Level1

---
+++ Entities/Player/Player.gd
@@ -0,0 +1,23 @@
+class_name Player
+extends CharacterBody2D
+
+# Stats
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("Left", "Right")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
