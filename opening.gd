a804883f3c5645538919708e6ebcb4820fad09a7
regen=Q <keannyooi@gmail.com>
2024-10-27 00:52:27 +0800

    remade first 5 levels + fixed opening transition

--- opening.gd
+++ opening.gd
@@ -19,8 +19,9 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		sequence_counter += 1
 		animation_player.play(opening_sequence[sequence_counter])
-	
-	if sequence_counter == 4:
-		await animation_player.animation_finished
-		get_tree().change_scene_to_file(MAIN_MENU_PATH)
+		
+		if sequence_counter == 4:
+			await animation_player.animation_finished
+			print(is_inside_tree())
+			get_tree().change_scene_to_file(MAIN_MENU_PATH)
 	
